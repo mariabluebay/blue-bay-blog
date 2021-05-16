fe8fb623d5f72b245d94578abc27e8e7ee7136f7
Maria Ursu <maria.ursu@bluebaytravel.co.uk>
2021-05-16 19:20:13 +0100

    returning the user's role

--- api/app/Http/Resources/User.php
+++ api/app/Http/Resources/User.php
@@ -15,8 +15,10 @@ class User extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'username' => $this->username,
             'email' => $this->email,
+            'role' => $this->role,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at
         ];
