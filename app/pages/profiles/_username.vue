42d6f831feb5b8ef0ceede596735c9a72dd37564
Maria Ursu <maria.ursu@bluebaytravel.co.uk>
2021-05-18 09:10:04 +0100

    placeholder for profile page

---
+++ app/pages/profiles/_username.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <img
+      :alt="account.name"
+      :src="account.cover"
+      class="w-full"/>
+    <div>
+      <img :alt="account.name"
+           :src="account.avatar"
+           class=""
+           width="150px"
+      />
+    </div>
+    <div >
+      <h3 >{{ account.name }}</h3>
+      <div >
+        <div>
+          <h2>{{ account.posts_count }}</h2>
+          <span>Posts</span>
+        </div>
+        <div >
+          <h2>{{ account.followers_count }}</h2>
+          <span>Friends</span>
+        </div>
+        <div >
+          <h2>{{ account.follows }}</h2>
+          <span>Comments</span>
+        </div>
+      </div>
+      <p class="mt-2 font-sans font-light text-grey-dark text-sm text-justify">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
+        a type specimen book.
+      </p>
+    </div>
+    <div>
+      <div> Joined {{ account.created_at }}</div>
+      <div>
+        <!-- follow button -->
+      </div>
+    </div>
+    <hr class="block py-4 mt-4">
+  </div>
+</template>
+<script>
+export default {
+
+  async asyncData({$content, $axios, params}) {
+    const {data} = await $axios.$get(`/profiles/${params.username}`);
+    return {account : data}
+  },
+
+}
+</script>
