a8de4d7d6fc73463a419ad0b4e9cac34c2a2f46e
Maria Ursu <maria.ursu@bluebaytravel.co.uk>
2021-05-19 11:45:38 +0100

    displaying the friend list on the authenticated users profile

--- app/components/ProfileCard.vue
+++ app/components/ProfileCard.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="card">
+  <div class="card full">
 
     <div class="card-image">
       <figure class="image is-2by1">
         <img :src="account.cover"
              alt="Profile Cover"
-             class="is-cover">
+             class="is-cover"
+        >
       </figure>
     </div>
 
@@ -15,7 +16,8 @@
           :src="account.avatar"
           :alt="account.username"
           class=" is-rounded rounded-full border-solid border-white border-2 w-12 h-12 object-cover"
-          id="profilePicture"/>
+          id="profilePicture"
+        />
       </figure>
     </div>
 
@@ -24,9 +26,9 @@
       <slot></slot>
 
       <h3 class="title is-2 is-spaced bd-anchor-title has-text-centered">
-                <span class="is-upper">
-                 {{ account.name ? account.name : account.username }}
-                </span>
+          <span class="is-upper">
+           {{ account.name ? account.name : account.username }}
+          </span>
       </h3>
       <h4 class="subtitle is-4 has-text-centered has-text-grey">
         {{ account.about ?  account.about : 'No tagline yet.'}}
@@ -35,7 +37,7 @@
 
     <footer class="card-footer no-top-margin no-left-margin">
       <div class="card-footer-item has-text-centered is-flex-direction-column">
-        <h5> {{ account.followers_count + account.follows_count }} </h5>
+        <h5> {{ account.friends_count }} </h5>
         <p>  Friends </p>
       </div>
       <div class="card-footer-item is-flex-direction-column">
--- app/pages/profiles/index.vue
+++ app/pages/profiles/index.vue
@@ -1,19 +1,23 @@
 <template>
   <div>
-    <div class="columns is-mobile is-centered">
-      <div class="column is-half">
+    <div class="tile is-ancestor">
+      <div class="tile is-parent is-2 is-vertical">
+        <FriendsList :friends="user.pending_friend_request" :pending="true"/>
+        <FriendsList :friends="user.friends" :pending="false"/>
+      </div>
+      <div class="tile is-parent is-6">
         <ProfileCard :account="user"/>
       </div>
     </div>
   </div>
-
 </template>
 <script>
 import PendingRequest from "../../components/PendingRequest";
 import ProfileCard from "../../components/ProfileCard";
+import FriendsList from "../../components/FriendsList";
 
 export default {
-  components: {ProfileCard, PendingRequest },
+  components: {FriendsList, ProfileCard, PendingRequest },
   middleware: ['auth'],
 }
 </script>
